3dfbda1ec27f61c88403af447196225425d3b49c
Eduard Tokarev <etdevios@gmail.com>
2023-01-28 19:13:57 +0300

    Initial Commit

---
+++ Magic 8 Ball (SwiftUI)/Magic_8_Ball__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_8_Ball__SwiftUI_App.swift
+//  Magic 8 Ball (SwiftUI)
+//
+//  Created by Eduard Tokarev on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Magic_8_Ball__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
